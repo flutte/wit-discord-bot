8ab7b8fee1cd89cb4b1526a327ccddc8301f87d8
BuildTools <unconfigured@null.spigotmc.org>
2020-05-05 16:34:12 +0200

    Reinitialize repo because of leak

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ with open("responses.json") as json_file:
 #DECLARE VARIABLES
 BOT_CHANNEL_ID = 0 #channel id here, it's an int btw
 WIT_TOKEN = 'Z2HRFYADTY3OPUFWTKWZ2C6UJFVES4H7' #You can replace this token, but by doing that my bot won't be able to learn
-DISCORD_TOKEN = 'DISCORD TOKEN GOES HERE'
+DISCORD_TOKEN = 'discord token here'
 
 #CREATE BOT INSTANCES
 bot = discord.Client()
@@ -15,17 +15,17 @@ client = Wit(WIT_TOKEN)
 
 #THIS WILL ONLY WORK ON PYTHON 3.6+, sorry
 def get_bot_response(response):
+	error_message = "I yet don't know how to respond, but by sending messages you help me learn! Thanks for contributing!"
 	global responses
 
 	try:
 		entity_response = next(iter(response['entities']))
-	except StopIteration:
-		return "I yet don't know how to respond, but by sending messages you help me learn! Thanks for contributing!"
+		if responses[str(entity_response)]:
+			return random.choice(responses[entity_response])
+	except:
+		return error_message
 
-	if responses[str(entity_response)]:
-		return random.choice(responses[entity_response])
-	else:
-		return "Error"
+	return error_message
 
 @bot.event
 async def on_ready():
@@ -38,5 +38,3 @@ async def on_message(message):
 		await message.channel.send(get_bot_response(resp))
 
 bot.run(DISCORD_TOKEN)
-
-#https://www.youtube.com/watch?v=qb_hqexKkw8
